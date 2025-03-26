5a9524dd24404245c93b2de5fb3734a4daaa6324
coff33ninja <coff33ninja69@gmail.com>
2025-03-26 06:51:27 +0200

    Fixed admin obtaining method

--- setup_venv.bat
+++ setup_venv.bat
@@ -5,16 +5,56 @@ setlocal
 echo This script requires admin privileges to run network tools.
 echo If prompted, please accept the UAC dialog to continue.
 
-:: Check for admin rights
-net session >nul 2>&1
-if %errorlevel% neq 0 (
-    echo Requesting admin rights...
-    echo Set UAC = CreateObject("Shell.Application") > "%temp%\getadmin.vbs"
-    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
-    "%temp%\getadmin.vbs"
-    del "%temp%\getadmin.vbs"
-    exit /b
-)
+@echo OFF
+setlocal
+%SystemRoot%\System32\rundll32.exe shell32.dll,SHCreateLocalServerRunDll {c82192ee-6cb5-4bc0-9ef0-fb818773790a}
+CLS
+MD %USERPROFILE%\AppData\Local\Temp\AIO
+echo. > %USERPROFILE%\AppData\Local\Temp\AIO\log.txt
+
+::========================================================================================================================================
+::========================================================================================================================================
+
+cls
+:SelfAdminTest
+ECHO.
+ECHO =============================
+ECHO Running Admin shell
+ECHO =============================
+
+:init
+setlocal DisableDelayedExpansion
+set "batchPath=%~0"
+for %%k in (%0) do set batchName=%%~nk
+set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+setlocal EnableDelayedExpansion
+
+:checkPrivileges
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+ECHO.
+ECHO **************************************
+ECHO Invoking UAC for Privilege Escalation
+ECHO **************************************
+
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+ECHO args = "ELEV " >> "%vbsGetPrivileges%"
+ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+ECHO Next >> "%vbsGetPrivileges%"
+ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+"%SystemRoot%\System32\WScript.exe" "%vbsGetPrivileges%" %*
+exit /B
+
+:gotPrivileges
+setlocal & pushd .
+cd /d %~dp0
+if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+
+::========================================================================================================================================
 
 :: Use %~dp0 to get the directory of the batch file
 set "SCRIPT_DIR=%~dp0"
